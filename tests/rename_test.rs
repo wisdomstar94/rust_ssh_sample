b117e4f7ad27060e897ee271dc6345da7ca7a119
wisdomstar94 <wisdomstar94@gmail.com>
2024-01-08 17:11:56 +0900

    rename 관련 테스트 코드 추가.

--- tests/rename_test.rs
+++ tests/rename_test.rs
@@ -20,6 +20,22 @@ fn file_rename_test() {
 } 
 
 #[test]
+fn file_rename_test2() {
+  dotenv().ok();
+  if let (Ok(user_name), Ok(host), Ok(port), Ok(private_key_path)) = (dotenv::var("SSH_USERNAME"), dotenv::var("SSH_HOST"), dotenv::var("SSH_PORT"), dotenv::var("SSH_PRIVATE_KEY_PATH")) {
+    let conn = ConnectInfo::new(user_name, host, port, private_key_path);
+    let sess = conn.try_connect().unwrap();
+    println!("ssh 연결 성공!");
+
+    let before_file_path = Path::new("/home/ec2-user/my_script.sh");
+    let after_file_path = Path::new("/home/ec2-user/my_script-modify.sh");
+    file_or_folder_rename(&sess, before_file_path, after_file_path).unwrap();
+
+    sess.disconnect(None, "test end", None).unwrap();
+  }
+} 
+
+#[test]
 fn folder_rename_test() {
   dotenv().ok();
   if let (Ok(user_name), Ok(host), Ok(port), Ok(private_key_path)) = (dotenv::var("SSH_USERNAME"), dotenv::var("SSH_HOST"), dotenv::var("SSH_PORT"), dotenv::var("SSH_PRIVATE_KEY_PATH")) {
@@ -29,6 +45,22 @@ fn folder_rename_test() {
 
     let before_file_path = Path::new("/home/ec2-user/kkk");
     let after_file_path = Path::new("/home/ec2-user/kkk3/kkk2/kkk");
+    file_or_folder_rename(&sess, before_file_path, after_file_path).unwrap();
+
+    sess.disconnect(None, "test end", None).unwrap();
+  }
+} 
+
+#[test]
+fn folder_rename_test2() {
+  dotenv().ok();
+  if let (Ok(user_name), Ok(host), Ok(port), Ok(private_key_path)) = (dotenv::var("SSH_USERNAME"), dotenv::var("SSH_HOST"), dotenv::var("SSH_PORT"), dotenv::var("SSH_PRIVATE_KEY_PATH")) {
+    let conn = ConnectInfo::new(user_name, host, port, private_key_path);
+    let sess = conn.try_connect().unwrap();
+    println!("ssh 연결 성공!");
+
+    let before_file_path = Path::new("/home/ec2-user/kkk3");
+    let after_file_path = Path::new("/home/ec2-user/kkk3-modify");
     file_or_folder_rename(&sess, before_file_path, after_file_path).unwrap();
 
     sess.disconnect(None, "test end", None).unwrap();
